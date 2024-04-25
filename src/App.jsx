d3b8f1a25a7c65b5e0d97e75ff1a1f3ec22f523d
Gustavo Alves da Silva <gustavoboygame@gmail.com>
2024-04-25 12:10:31 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import Eventos from './componentes/Eventos'
+import PrimeiroComponente from './componentes/PrimeiroComponente'
+
+function App() {
+ 
+
+  return (
+   
+
+    <div>
+      <h1>Fundamentos Reacts</h1>
+
+      <PrimeiroComponente />
+
+      <Eventos/>
+    </div>
+  )
+}
+
+export default App
